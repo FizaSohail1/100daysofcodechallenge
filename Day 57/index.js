210edacd0c1e3f652ed1af9d0ef16383149979dc
FizaSohail1 <159183796+FizaSohail1@users.noreply.github.com>
2024-05-05 03:24:00 +0500

    Completed the challenge of day 57

---
+++ Day 57/index.js
@@ -0,0 +1,18 @@
+// question : What will be the result for these expressions?
+
+5 > 4
+"apple" > "pineapple"
+"2" > "12"
+undefined == null
+undefined === null
+null == "\n0\n"
+null === +"\n0\n"
+
+// Solution
+5 > 4     // true
+"apple" > "pineapple"  // false
+"2" > "12"    //true
+undefined == null //true
+undefined === null  //false
+null == "\n0\n" //false
+null === +"\n0\n"  //false
