40a0ac2ebadfc22922ec194582b4913c722adef0
FizaSohail1 <159183796+FizaSohail1@users.noreply.github.com>
2024-05-08 00:33:27 +0500

    Completed the challenge of day 58

---
+++ Day 58/Task5.js
@@ -0,0 +1,15 @@
+//Write an if condition to check that age is between 14 and 90 inclusively.“Inclusively” means that age can reach the edges 14 or 90.
+
+if(age>= 40 && age <=90){
+    console.log(age)
+}
+// Write an if condition to check that age is NOT between 14 and 90 inclusively.Create two variants: the first one using NOT !, the second one – without it.
+
+//first varient
+if (!(age >= 14 && age <= 90)){
+
+}
+//secound varient
+if (age < 14 || age > 90){
+    console.log()
+}
