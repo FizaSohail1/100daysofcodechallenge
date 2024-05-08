fcf7411f328be9adaab6e8a24bdd908913302551
FizaSohail1 <159183796+FizaSohail1@users.noreply.github.com>
2024-05-08 21:44:11 +0500

    Completed the challenge of day 60

---
+++ Day 60/index.js
@@ -0,0 +1,11 @@
+// Task 1
+// Write a function min(a,b) which returns the least of two numbers a and b.
+function min (a,b){
+    if(a < b){
+        return a;
+    }
+    else{
+        return b;
+    }
+}
+console.log(min(4,10));
