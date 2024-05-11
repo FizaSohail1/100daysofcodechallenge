b191b3a14f2805c00c2923ec9fc130b1d28852f1
FizaSohail1 <159183796+FizaSohail1@users.noreply.github.com>
2024-05-11 22:02:24 +0500

    Completed the challenege of day 69

---
+++ Day 69/index.ts
@@ -0,0 +1,13 @@
+// Enums
+
+enum Days {
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+} 
+const today: Days = Days.Wednesday;
+console.log(`Today is ${Days[today]}`);
