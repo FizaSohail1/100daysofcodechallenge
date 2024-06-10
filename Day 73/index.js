834a4b86ba040f30b04c32b45e780f67bf5a96da
FizaSohail1 <159183796+FizaSohail1@users.noreply.github.com>
2024-06-11 04:00:15 +0500

    completed the challenge of day 73

---
+++ Day 73/index.js
@@ -0,0 +1,5 @@
+"use strict";
+// Type Assertion
+let data = 1000;
+// Type assertion to treat "data" as a string
+console.log(data.repeat(3));
---
+++ Day 73/index.ts
@@ -0,0 +1,6 @@
+// Type Assertion
+
+let data: any = 1000;
+
+// Type assertion to treat "data" as a string
+console.log((data as string).repeat(3));
